21bfa15c2896d2de27409e35820321fea4156582
Praveen P <ppraveentr@gmail.com>
2022-09-04 01:52:08 -0700

    Initial commit

---
+++ ExampleApp/ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,22 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by Praveen Prabhakar on 03/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    
+    /// setup on app launch
+//    init() {
+//    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
